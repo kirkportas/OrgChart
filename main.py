ee127324ffce26e3d41e73d15d1287e330a4549e
Karel Ha <mathemage@gmail.com>
2017-03-13 22:46:06 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def load_csv(orgchart_filename, employees_filename):
+    print("Loading orgchart file %s..." % orgchart_filename)
+    print("Loading employees file %s..." % employees_filename)
+    return
+
+if __name__ == "__main__":
+    orgchart_filename = "orgchart-data.csv"
+    employees_filename = "employees-data.csv"
+    load_csv(orgchart_filename, employees_filename)
