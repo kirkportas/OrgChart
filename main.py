db05f13f52210358a9da347aaba6e5ab77715cd0
Karel Ha <mathemage@gmail.com>
2017-03-13 23:22:26 +0100

    Open CSV files for reading
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import csv
+
+
 def load_csv(orgchart_filename, employees_filename):
+    # TODO check for invalid filenames
     print("Loading orgchart file %s..." % orgchart_filename)
+    with open(orgchart_filename, encoding='cp1250') as orgchart_file:
+        orgchart_reader = csv.reader(orgchart_file, delimiter=';')
+        for row in orgchart_reader:
+            print(', '.join(row))
     print("Loading employees file %s..." % employees_filename)
+    with open(employees_filename, encoding='cp1250') as employees_file:
+        employees_reader = csv.reader(employees_file, delimiter=';')
+        for row in employees_reader:
+            print('. '.join(row))
     return
+
 
 if __name__ == "__main__":
     orgchart_filename = "orgchart-data.csv"
