462c243e1cf3d321a642275dc2f897cfd148b150
Karel Ha <mathemage@gmail.com>
2017-03-14 10:49:03 +0100

    Outline user commands and extract command argument
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ def load_csv(orgchart_filename, employees_filename):
     return
 
 
+def get_int(command):
+    args = command.split()
+    if len(args) != 2:
+        return None
+    try:
+        # TODO check for valid range of id_dept
+        return int(command.split()[1])
+    except ValueError:
+        return None
+
+
 def main():
     if len(sys.argv) < 3:
         print("Not enough arguments!")
@@ -25,7 +36,31 @@ def main():
     orgchart_filename = sys.argv[1]
     employees_filename = sys.argv[2]
     load_csv(orgchart_filename, employees_filename)
-
+    while True:
+        command = input("User command: ")
+        # TODO check for invalid commands
+        if command == "Exit":
+            break
+        dept_id = get_int(command)
+        if dept_id is None:
+            # TODO show this error only in combination with a valid command
+            print("Invalid number argument!")
+            continue
+        if command.startswith("Department"):
+            # TODO implement Department
+            print("Show department %d" % dept_id)
+        elif command.startswith("Count"):
+            # TODO implement Count
+            print("Count employees in department %d" % dept_id)
+        elif command.startswith("People"):
+            # TODO implement People
+            print("Employees of department %d" % dept_id)
+        elif command.startswith("Avgage"):
+            # TODO implement Avgage
+            print("Average age of employees in department %d" % dept_id)
+        else:
+            print("Invalid command!")
+            # TODO help message with list of commands
 
 if __name__ == "__main__":
     main()
