e0126b63c46a4a638d83b1b469ebf5ab9f66b2f2
Karel Ha <mathemage@gmail.com>
2017-03-14 15:20:52 +0100

    Create class Department
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- main.py
+++ main.py
@@ -3,13 +3,36 @@ import csv
 import sys
 
 
+class Department:
+    _sub_depts = []
+    _employees = []
+
+    def __init__(self, dept_id, parent_id, name, city):
+        self._id = dept_id
+        self._parent_id = parent_id
+        self._name = name
+        self._city = city
+
+    def __repr__(self):
+        return ("%d (%s), %s, %s" % (self._id, str(self._parent_id), self._name, self._city))
+
+departments = {}
+
+
 def load_csv(orgchart_filename, employees_filename):
+    global departments
     # TODO check for invalid filenames
     print("Loading orgchart file %s..." % orgchart_filename)
     with open(orgchart_filename, encoding='cp1250') as orgchart_file:
         orgchart_reader = csv.reader(orgchart_file, delimiter=';')
         for row in orgchart_reader:
-            print(', '.join(row))
+            dept_id, parent_id, name, city = row
+            # TODO verify correct CSV format
+            dept_id = int(dept_id)
+            if parent_id != '':
+                parent_id = int(parent_id)
+            departments[dept_id] = Department(dept_id, parent_id, name, city)
+            print(departments[dept_id])
     print("Loading employees file %s..." % employees_filename)
     with open(employees_filename, encoding='cp1250') as employees_file:
         employees_reader = csv.reader(employees_file, delimiter=';')
@@ -41,11 +64,13 @@ def main():
         # TODO check for invalid commands
         if command == "Exit":
             break
+
         dept_id = get_int(command)
         if dept_id is None:
             # TODO show this error only in combination with a valid command
             print("Invalid number argument!")
             continue
+
         if command.startswith("Department"):
             # TODO implement Department
             print("Show department %d" % dept_id)
