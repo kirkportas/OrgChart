63d41e8314394cb89cf47f81abea468a69473a22
Karel Ha <mathemage@gmail.com>
2017-03-15 22:20:40 +0100

    Split command_with_arg into command and dept_id
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- main.py
+++ main.py
@@ -25,24 +25,24 @@ def main():
     org.load_csv(orgchart_filename, employees_filename)
     departments = org.get_departments()
     while True:
-        command = input("User command: ")
+        command_with_arg = input("User command: ")
+        command = command_with_arg.split()[0]
         if command not in list_of_commands:
-            print("Invalid command!")
-            # TODO help message with list of commands
+            print("Invalid command! Supported commands: " + ', '.join(list_of_commands))
+            continue
+
+        if command not in ["Exit"]:                         # hack: for commands with 1 number argument
+            dept_id = get_int(command_with_arg)
+            if dept_id is None:
+                print("Invalid number argument!")
+                continue
+            elif dept_id not in org.get_departments().keys():
+                print("Invalid department ID!")
+                continue
 
         if command == "Exit":
             break
-
-        dept_id = get_int(command)
-        if dept_id is None:
-            # TODO show this error only in combination with a valid command
-            print("Invalid number argument!")
-            continue
-        elif dept_id not in org.get_departments().keys():
-            print("Invalid department ID!")
-            continue
-
-        if command.startswith("Department"):
+        elif command.startswith("Department"):
             departments[dept_id].display_department()
         elif command.startswith("Count"):
             print(departments[dept_id].count_employees())
