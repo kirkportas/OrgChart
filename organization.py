1c099d5406ffc8558e83063dc29f497b1c638728
Karel Ha <mathemage@gmail.com>
2017-03-16 15:18:23 +0100

    Check for non-existing parent ID and create alternative Department constructors
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- organization.py
+++ organization.py
@@ -30,12 +30,18 @@ class Organization:
                     dept_id = int(dept_id)
                     if parent_id != '':
                         parent_id = int(parent_id)
-                        # TODO check for non-existing parent_id
+                        if parent_id not in self._departments:
+                            self._departments[parent_id] = Department.from_dept_id(self, parent_id)
                         self._departments[parent_id].add_subdept(dept_id)
                 except ValueError:
                     print("Wrong format in %s!" % orgchart_filename)
                     sys.exit()
-                self._departments[dept_id] = Department(self, dept_id, parent_id, name, city)
+                existing_sub_depts = []                     # backup self._departments to copy to a new instance
+                if dept_id in self._departments:
+                    existing_sub_depts = self._departments[dept_id].get_sub_depts()[:]
+                self._departments[dept_id] = Department.from_CSV_row(self, dept_id, parent_id, name, city)
+                for sub_dept_id in existing_sub_depts:
+                    self._departments[dept_id].add_subdept(sub_dept_id)
 
     def load_employees_csv(self, employees_filename):
         with open(employees_filename, encoding='utf-8') as employees_file:
@@ -57,9 +63,6 @@ class Organization:
 
 class Department:
     def __init__(self, organization, dept_id, parent_id, name, city):
-        assert isinstance(dept_id, int)
-        assert isinstance(parent_id, int) or parent_id == ''
-
         self._organization = organization
         self._dept_id = dept_id
         self._parent_id = parent_id if parent_id != '' else None
@@ -67,6 +70,20 @@ class Department:
         self._city = city
         self._sub_depts = []
         self._employees = []
+
+    @classmethod
+    def from_CSV_row(cls, organization, dept_id, parent_id, name, city):
+        assert isinstance(dept_id, int)
+        assert isinstance(parent_id, int) or parent_id == ''
+
+        dept = cls(organization, dept_id, parent_id, name, city)
+        dept._parent_id = parent_id if parent_id != '' else None
+        return dept
+
+    @classmethod
+    def from_dept_id(cls, organization, dept_id):
+        dept = cls(organization, dept_id, None, None, None)
+        return dept
 
     def __repr__(self):
         representation = "[%d (%s), %s, %s]" % (self._dept_id, str(self._parent_id), self._name, self._city)
@@ -81,6 +98,9 @@ class Department:
     def add_subdept(self, sub_dept_id):
         if sub_dept_id not in self._sub_depts:
             self._sub_depts.append(sub_dept_id)
+
+    def get_sub_depts(self):
+        return self._sub_depts
 
     def display_department(self):
         print("%s, %s" % (self._name, self._city))
